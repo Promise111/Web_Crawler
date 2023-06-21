33cc55e7df60faaccd23fb39a557b279ddd3d04e
Promise <promiseihunna@gmail.com>
2023-06-21 16:48:35 +0100

    gitignored admin.json

--- dbWorker.js
+++ dbWorker.js
@@ -3,14 +3,16 @@ const admin = require("firebase-admin");
 const path = require("node:path");
 
 let firebaseConfig = {
-  credential: admin.credential.cert(path.join(__dirname, "admin.json")),
-//   apiKey: "AIzaSyB2xkaT1wWn4EWHEk206nUTKnLtfi4klco",
-//   authDomain: "web-worker1.firebaseapp.com",
-//   databaseURL: "https://web-worker1.firebaseio.com",
-//   projectId: "web-worker1",
-//   storageBucket: "web-worker1.appspot.com",
-//   messagingSenderId: "390530352979",
-//   appId: "1:390530352979:web:9663e416c36224ed6a03a6",
+  credential: admin.credential.cert(
+    path.join(__dirname, "firebase", "admin.json")
+  ),
+  //   apiKey: "AIzaSyB2xkaT1wWn4EWHEk206nUTKnLtfi4klco",
+  //   authDomain: "web-worker1.firebaseapp.com",
+  //   databaseURL: "https://web-worker1.firebaseio.com",
+  //   projectId: "web-worker1",
+  //   storageBucket: "web-worker1.appspot.com",
+  //   messagingSenderId: "390530352979",
+  //   appId: "1:390530352979:web:9663e416c36224ed6a03a6",
 };
 
 admin.initializeApp(firebaseConfig);
